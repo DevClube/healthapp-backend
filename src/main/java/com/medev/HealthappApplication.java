d7ae6050b9dd7f7f01d9f13e48c041da2b72857c
mehdi.dev <ramzimed302@gmail.com>
2023-12-09 17:14:12 +0100

    first commit

---
+++ src/main/java/com/medev/HealthappApplication.java
@@ -0,0 +1,38 @@
+package com.medev;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.medev.models.ApplicationUser;
+import com.medev.models.Role;
+import com.medev.repository.RoleRepository;
+import com.medev.repository.UserRepository;
+
+@SpringBootApplication
+public class HealthappApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(HealthappApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner run(RoleRepository roleRepository, UserRepository userRepository, PasswordEncoder passwordEncode){
+		return args ->{
+			if(roleRepository.findByAuthority("ADMIN").isPresent()) return;
+			Role adminRole = roleRepository.save(new Role("ADMIN"));
+			roleRepository.save(new Role("USER"));
+
+			Set<Role> roles = new HashSet<>();
+			roles.add(adminRole);
+
+			ApplicationUser admin = new ApplicationUser(1, "admin", passwordEncode.encode("password"), roles);
+
+			userRepository.save(admin);
+		};
+	}
+}
---
+++ src/main/java/com/medev/repository/RoleRepository.java
@@ -0,0 +1,13 @@
+package com.medev.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.medev.models.Role;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Integer>{
+    Optional<Role> findByAuthority(String authority);
+}
